221bf00c31abd8e42c7cbb77da3f20c749a42a0d
jmburton13 <jmburton13@gmail.com>
2021-02-03 15:44:27 -0500

    "Initial testing passed for forward and reverse"

---
+++ Pig Latin Translator/src/main/java/org/translator/Translator.java
@@ -0,0 +1,35 @@
+package org.translator;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class Translator
+{
+    public static void main( String[] args )
+    {
+        //CONSTRUCTORS
+        WelcomeBanner welcome = new WelcomeBanner();
+        Scanner scanner = new Scanner(System.in);
+        Converter converter = new Converter();
+
+        //WELCOME AND USER SELECT TRANSLATION DIRECTION
+        System.out.println( welcome.banner );
+        System.out.println("  [1] Language --> PigLatin\n  [2] PigLatin --> Language\nPlease select your translator direction: ");
+        String keyboardInput = scanner.nextLine().toLowerCase();
+
+        boolean isToPigLatin = false;
+        if (keyboardInput.toLowerCase().equals("1")) { isToPigLatin = true;}
+
+        //USER INPUT OF STRING FOR TRANSLATION
+        System.out.println("Thanks! Please type what you would like to have translated (exclude punctuation): ");
+        String stringToConvert = scanner.nextLine();
+
+        String printableTranslation = converter.convert(stringToConvert, isToPigLatin);
+        System.out.println(printableTranslation);
+        }
+    }
+
